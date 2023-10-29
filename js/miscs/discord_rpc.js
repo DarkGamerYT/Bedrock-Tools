0ed4f4f631c9eae16360e2522ca68314716eefdd
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-10-29 12:23:51 +0000

    Disabled Discord RPC on Linux due to a bug

--- js/miscs/discord_rpc.js
+++ js/miscs/discord_rpc.js
@@ -1,35 +1,37 @@
-const RPC = new (require("discord-rpc")).Client({ transport: "ipc" });
-RPC.on(
-    "ready", async () => {
-        console.log( RPC.user );
-        let lastRoute;
-        let lastTime;
-        setInterval(
-            () => {
-                const routeName = router.history.list[router.history.list.length - 1];
-                const route = router.routes.find((r) => r.route == routeName);
-                
-                if (settings.get( "discordrpc" )) {
-                    if (lastRoute != routeName) {
-                        lastTime = Date.now();
-                        RPC.setActivity(
-                            {
-                                details: localisation.translate(route?.name ?? "bedrocktools.screen.unknown"),
-                                state: "Route: " + (route?.route ?? routeName),
-                                startTimestamp: lastTime,
-                                largeImageKey: "icon",
-                                largeImageText: "Bedrock Tools v" + BedrockTools.version,
-                                smallImageKey: route?.metadata?.rpc,
-                                smallImageText: route?.name,
-                            },
-                        );
-                    };
-                } else RPC.clearActivity();
-                lastRoute = routeName;
-            }, 5000,
-        );
-    },
-);
+if (process.platform.toLowerCase() != "linux") {
+    const RPC = new (require("discord-rpc")).Client({ transport: "ipc" });
+    RPC.on(
+        "ready", async () => {
+            console.log( RPC.user );
+            let lastRoute;
+            let lastTime;
+            setInterval(
+                () => {
+                    const routeName = router.history.list[router.history.list.length - 1];
+                    const route = router.routes.find((r) => r.route == routeName);
+                    
+                    if (settings.get( "discordrpc" )) {
+                        if (lastRoute != routeName) {
+                            lastTime = Date.now();
+                            RPC.setActivity(
+                                {
+                                    details: localisation.translate(route?.name ?? "bedrocktools.screen.unknown"),
+                                    state: "Route: " + (route?.route ?? routeName),
+                                    startTimestamp: lastTime,
+                                    largeImageKey: "icon",
+                                    largeImageText: "Bedrock Tools v" + BedrockTools.version,
+                                    smallImageKey: route?.metadata?.rpc,
+                                    smallImageText: route?.name,
+                                },
+                            );
+                        };
+                    } else RPC.clearActivity();
+                    lastRoute = routeName;
+                }, 5000,
+            );
+        },
+    );
 
-const startRPC = () => RPC.login({ clientId : "1144685291014213802" }).catch(() => setTimeout(() => startRPC(), 5000));
-startRPC();
+    const startRPC = () => RPC.login({ clientId : "1144685291014213802" }).catch(() => setTimeout(() => startRPC(), 5000));
+    startRPC();
+};
