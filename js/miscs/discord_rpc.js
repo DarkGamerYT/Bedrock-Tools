b87c209f257550f789884b66eca0e85fb5e3e325
King <47607185+DarkGamerYT@users.noreply.github.com>
2024-01-04 16:17:57 +0100

    Clean up

--- js/miscs/discord_rpc.js
+++ js/miscs/discord_rpc.js
@@ -1,38 +1,41 @@
-if (process.platform.toLowerCase() != "linux") {
-    const RPC = new (require("discord-rpc")).Client({ transport: "ipc" });
-    RPC.on(
-        "ready", async () => {
-            let lastRoute;
-            let lastTime;
-            setInterval(
-                () => {
-                    const routeName = router.history.list[router.history.list.length - 1];
-                    const route = router.routes.find((r) => r.route == routeName);
+const discordRPC = require("discord-rpc");
+const rpc = new discordRPC.Client({ transport: "websocket" });
+rpc.on(
+    "ready", async (data) => {
+        let lastRoute;
+        let lastTime;
+        console.log(data);
+        setInterval(
+            () => {
+                const routeName = router.history.list[router.history.list.length - 1];
+                const route = router.routes.find((r) => r.route == routeName);
                     
-                    if (settings.get( "discordrpc" )) {
-                        if (lastRoute != routeName) {
-                            lastTime = Date.now();
-                            const screenName = localisation.translate(
-                                route?.name ?? "bedrocktools.screen.unknown"
-                            );
+                if (settings.get( "discordrpc" )) {
+                    if (lastRoute != routeName) {
+                        lastTime = Date.now();
+                        const screenName = localisation.translate(
+                            route?.name ?? "bedrocktools.screen.unknown"
+                        );
 
-                            RPC.setActivity({
-                                details: screenName,
-                                state: "Route: " + (route?.route ?? routeName),
-                                startTimestamp: lastTime,
-                                largeImageKey: "icon",
-                                largeImageText: "Bedrock Tools v" + BedrockTools.version,
-                                smallImageKey: route?.metadata?.rpc,
-                                smallImageText: screenName,
-                            });
-                        };
-                    } else RPC.clearActivity();
-                    lastRoute = routeName;
-                }, 5000,
-            );
-        },
-    );
+                        rpc.setActivity({
+                            details: screenName,
+                            state: "Route: " + (route?.route ?? routeName),
+                            startTimestamp: lastTime,
+                            largeImageKey: "icon",
+                            largeImageText: "Bedrock Tools v" + BedrockTools.version,
+                            smallImageKey: route?.metadata?.rpc,
+                            smallImageText: screenName,
+                        });
+                    };
+                } else RPC.clearActivity();
+                lastRoute = routeName;
+            }, 5000,
+        );
+    },
+);
 
-    const startRPC = () => RPC.login({ clientId : "1144685291014213802" }).catch(() => setTimeout(() => startRPC(), 5000));
-    startRPC();
+startRPC();
+function startRPC() {
+    rpc.login({ clientId : "1144685291014213802" })
+    .catch(() => setTimeout(() => startRPC(), 5000));
 };
--- js/routes/settings.js
+++ js/routes/settings.js
@@ -16,62 +16,67 @@ const Settings = {
         let panoramaEnabled = settings.get( "panorama_enabled" );
         let panorama = settings.get( "panorama" );
         const langs = localisation.getLangs();
+        let elements = [];
+
+        elements.push(Components.createElement("switch", {
+            label: "Sidebar on the right",
+            description: "Places the sidebar to right side of the screen",
+            id: "right",
+            toggled: isRight,
+            onChange: SettingsRouteUtils.toggleRight,
+        }));
+
+        if (process.platform.toLowerCase() != "linux") {
+            elements.push(Components.createElement("switch", {
+                label: "Discord Rich Presence",
+                description: "Shows that you're using Bedrock Tools on your Discord profile",
+                id: "discordrpc",
+                toggled: discordRpc,
+                onChange: SettingsRouteUtils.toggleDiscordRpc,
+            }));
+        };
+        
+        elements.push(Components.createElement("switch", {
+            label: "Animations",
+            description: "Enables/Disables the animations",
+            id: "animations",
+            toggled: animations,
+            onChange: SettingsRouteUtils.toggleAnimations,
+        }));
+        
+        elements.push(Components.createElement("dropdown", {
+            label: "Language",
+            description: "Changes the app's display language",
+            id: "language",
+            inline: true,
+            selected: langs.indexOf(locale),
+            items: langs,
+            onChange: SettingsRouteUtils.changeLanguage,
+        }));
+        
+        elements.push(Components.createElement("switch", {
+            label: "Panorama Enabled (Restart required)",
+            description: "Enables/Disables the panorama",
+            id: "panoramaEnabled",
+            toggled: panoramaEnabled,
+            onChange: SettingsRouteUtils.togglePanorama,
+        }));
+
+        elements.push(Components.createElement("dropdown", {
+            label: "Panoramas (Restart required)",
+            description: "Changes the app's panorama",
+            id: "panorama",
+            inline: true,
+            selected: panoramas.indexOf(panorama),
+            items: panoramas,
+            onChange: SettingsRouteUtils.changePanorama,
+        }));
+
         return (
             Components.createHeader({ label: localisation.translate( "bedrocktools.screen.settings" ), back: true, settings: false })
             + (
                 `<div style="margin-top: 25px;margin-left: auto;margin-right: auto;width: 65%;">
-                    ${Components.createElements(
-                        {
-                            elements: [
-                                Components.createElement("switch", {
-                                    label: "Sidebar on the right",
-                                    description: "Places the sidebar to right side of the screen",
-                                    id: "right",
-                                    toggled: isRight,
-                                    onChange: SettingsRouteUtils.toggleRight,
-                                }),
-                                Components.createElement("switch", {
-                                    label: "Discord Rich Presence",
-                                    description: "Shows that you're using Bedrock Tools on your Discord profile",
-                                    id: "discordrpc",
-                                    toggled: discordRpc,
-                                    onChange: SettingsRouteUtils.toggleDiscordRpc,
-                                }),
-                                Components.createElement("switch", {
-                                    label: "Animations",
-                                    description: "Enables/Disables the animations",
-                                    id: "animations",
-                                    toggled: animations,
-                                    onChange: SettingsRouteUtils.toggleAnimations,
-                                }),
-                                Components.createElement("dropdown", {
-                                    label: "Language",
-                                    description: "Changes the app's display language",
-                                    id: "language",
-                                    inline: true,
-                                    selected: langs.indexOf(locale),
-                                    items: langs,
-                                    onChange: SettingsRouteUtils.changeLanguage,
-                                }),
-                                Components.createElement("switch", {
-                                    label: "Panorama Enabled (Restart required)",
-                                    description: "Enables/Disables the panorama",
-                                    id: "panoramaEnabled",
-                                    toggled: panoramaEnabled,
-                                    onChange: SettingsRouteUtils.togglePanorama,
-                                }),
-                                Components.createElement("dropdown", {
-                                    label: "Panoramas (Restart required)",
-                                    description: "Changes the app's panorama",
-                                    id: "panorama",
-                                    inline: true,
-                                    selected: panoramas.indexOf(panorama),
-                                    items: panoramas,
-                                    onChange: SettingsRouteUtils.changePanorama,
-                                }),
-                            ],
-                        },
-                    )}
+                    ${Components.createElements({ elements })}
                     ${Components.createElement("button", {
                         label: "Save Settings",
                         id: "saveSettings",
